d8fb072955dda69cdc8cc0ef9677ad836e536b46
David Ortiz <z.david.ortiz@gmail.com>
2014-04-29 20:14:41 +0200

    Added problem 6.

---
+++ 006.rb
@@ -0,0 +1,9 @@
+# Find the difference between the sum of the squares of the first one
+# hundred natural numbers and the square of the sum.
+
+sum = sum_of_squares = 0
+(1..100).each do |x|
+	sum += x
+	sum_of_squares += x**2
+end
+puts sum**2 - sum_of_squares
