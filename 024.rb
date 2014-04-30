897c3de3ca4a5de617c9fa8662b6f16fd53f92a0
David Ortiz <z.david.ortiz@gmail.com>
2014-04-30 20:42:39 +0200

    Added description for problem 24.

--- 024.rb
+++ 024.rb
@@ -1,3 +1,6 @@
+# What is the millionth lexicographic permutation of the digits 
+# 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
 digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 permutations = Array.new
 digits.permutation.to_a.map { |perm| permutations.push(perm.join) }
