fbe8850c4c8d1e6f48f9a21ea8ffd782289b0c28
David Ortiz <z.david.ortiz@gmail.com>
2014-05-01 00:21:10 +0200

    Added problem 97

---
+++ 097.rb
@@ -0,0 +1,4 @@
+# In 2004 there was found a massive non-Mersenne prime which contains 2,357,207
+# digits: 28433×27830457+1. Find the last ten digits of this prime number.
+
+puts (28433*(2**7830457)+1)%10000000000
