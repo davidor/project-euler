31bbd95f5a6d66cfb1196e7f95644950b7bbf408
David Ortiz <z.david.ortiz@gmail.com>
2014-05-03 16:34:26 +0200

    Revisited problem 20.

--- 020.rb
+++ 020.rb
@@ -1,8 +1,4 @@
 # Find the sum of the digits in the number 100!
 
 fact = (1..100).reduce(:*)
-sum_of_digits = 0
-fact.to_s.split("").each do |digit|
-    sum_of_digits += digit.to_i
-end
-puts sum_of_digits
+puts fact.to_s.split("").each.inject(0) { |sum, digit| sum += digit.to_i }
