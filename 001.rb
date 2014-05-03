51b193f803fe250a704d23bd707452f6765702da
David Ortiz <z.david.ortiz@gmail.com>
2014-05-03 16:26:31 +0200

    Revisited problem 1.

--- 001.rb
+++ 001.rb
@@ -1,7 +1,3 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
-(1...1000).each do |x|
-    sum += x if (x%3 == 0 or x%5 == 0)
-end
-puts sum
+puts (1...1000).select { |num| num%3 == 0 or num%5 == 0 }.reduce(:+)
