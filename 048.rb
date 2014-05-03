12dd325a7eb51f16c25aea7ef252c5f731740fe5
David Ortiz <z.david.ortiz@gmail.com>
2014-05-03 16:40:16 +0200

    Revisited problem 48.

--- 048.rb
+++ 048.rb
@@ -1,7 +1,4 @@
 #Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000
 
-sum = 0
-(1..1000).each do |x|
-    sum += x**x
-end
+sum = (1..1000).each.inject(0) { |sum, elem| sum + elem**elem }
 puts sum.to_s.split("").last(10).join
