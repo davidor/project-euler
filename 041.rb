fab0a25eea872c9d4a367104b79e0be497e7e36e
David Ortiz <z.david.ortiz@gmail.com>
2014-05-04 12:40:21 +0200

    Added problem 41.

---
+++ 041.rb
@@ -0,0 +1,12 @@
+# What is the largest n-digit pandigital prime that exists?
+
+require 'prime'
+
+(1..9).to_a.reverse.each do |digits|
+    permutations = (1..digits).to_a.reverse.permutation
+    result = permutations.detect { |perm| perm.join.to_i.prime? }
+    if not result.nil?
+        puts result.join
+        exit
+    end
+end
