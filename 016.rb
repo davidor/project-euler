0e8133b3041c59a1877b20d672698e2ac45da9f9
David Ortiz <z.david.ortiz@gmail.com>
2014-05-15 23:37:54 +0200

    Revisited problem 16.

--- 016.rb
+++ 016.rb
@@ -1,8 +1,3 @@
 # What is the sum of the digits of the number 2^1000 ?
 
-digits = (2**1000).to_s
-sum_of_digits = 0
-digits.split("").each do |digit|
-    sum_of_digits += digit.to_i
-end
-puts sum_of_digits
+puts (2**1000).to_s.split("").inject(0) { |sum, digit| sum += digit.to_i }
